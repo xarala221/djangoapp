c688e7039832cdb73024a3d7961ba3e921f53f69
xarala221 <xaralaxarala@gmail.com>
2018-04-14 11:25:41 +0000

    more field

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+LAYOUT_CHOICES = (
+    ('standard', 'Standard'),
+     ('stacked', 'Stacked')
+                ) 
+
 class Page(models.Model):
     title               = models.CharField(max_length=200)
     title_description   = models.CharField(max_length=200, blank=True, null=True)
     title_btn           = models.CharField(max_length=50, blank=True, null=True, default="Join")
     title_btn_url       = models.CharField(max_length=50, blank=True, null=True)
     content             = models.TextField()
+    show_nav            = models.BooleanField(default=True)
+    nav_color           = models.CharField(max_length=10, default="#000000")
+    layout              = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default="standard")
+    video_embed         = models.TextField()
+
+    def __str__(self):
+        return self.title
