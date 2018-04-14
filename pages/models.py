703df2f6d075be00236012a9570d0c8f56f76ea7
xarala221 <xaralaxarala@gmail.com>
2018-04-14 12:58:33 +0000

    standard and stacked page

--- pages/models.py
+++ pages/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 LAYOUT_CHOICES = (
     ('standard', 'Standard'),
      ('stacked', 'Stacked')
-                ) 
+                )
 
+def layout_validation(value):
+    if value[0] != '#':
+        raise ValidationError("Must begin with #")
+    if len(value) == 4 or len(value) == 7:
+        return value
+    raise ValidationError("Incorrect length")
 class Page(models.Model):
     title               = models.CharField(max_length=200)
     title_description   = models.CharField(max_length=200, blank=True, null=True)
@@ -14,9 +21,9 @@ class Page(models.Model):
     title_btn_url       = models.CharField(max_length=50, blank=True, null=True)
     content             = models.TextField()
     show_nav            = models.BooleanField(default=True)
-    nav_color           = models.CharField(max_length=10, default="#000000")
+    nav_color           = models.CharField(max_length=7, default="#000000", validators = [layout_validation])
     layout              = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default="standard")
-    video_embed         = models.TextField()
+    video_embed         = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
